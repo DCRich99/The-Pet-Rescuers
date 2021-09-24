2a7260ab1055eef92eb6dc9afd55a3d2d573543d
TannerCarter <tannerc.bootcamp@gmail.com>
2021-09-23 21:29:31 -0700

    connection js completed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,13 @@
-// Tanner input database connection here
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/the-pet-rescuers",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  }
+);
+
+module.exports = mongoose.connection;

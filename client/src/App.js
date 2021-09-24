cf260c92eaa9849c3a7226e17d069138dec6d31b
David C Richardson <81455373+DCRich99@users.noreply.github.com>
2021-09-24 11:56:34 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPets from "./pages/SearchPets";
+import SavedPets from "./pages/SavedPets";
+import DonatePets from "./pages/DonatePets";
 import Navbar from "./components/Navbar";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch></Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchPets} />
+            <Route exact path="/donate" component={DonatePets} />
+            <Route exact path="/saved" component={SavedPets} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 

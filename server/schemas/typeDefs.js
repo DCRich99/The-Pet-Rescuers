9d62e8f0040b52b94e0f2c25240da915650c4f90
TannerCarter <tannerc.bootcamp@gmail.com>
2021-09-25 14:55:27 -0700

    Implemented typeDefs, schema complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,49 @@
-//typeDefs here
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Pet {
+    petId: ID!
+    size: [String]
+    description: String
+    species: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input PetInput {
+    size: [String]
+    petId: ID!
+    description: String
+    species: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    petCount: Int
+    savedPets: [Pet]
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savePet(input: PetInput): User
+    removePet(petId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
